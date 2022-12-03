5621b7dc84a504f49798bb8ebb5f0c73be2a9015
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2022-12-03 15:26:36 +0200

    feat: make cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
+const { argv, stdout } = process;
+
 const parseArgs = () => {
     // Write your code here 
+    const args = argv;
+    args.forEach((currentValue, index, array) => {
+      if (currentValue.includes("--")) {
+        let output = `${currentValue.replace("--", "")} is ${array[index + 1]},`;
+        stdout.write(output + "\n");
+      }
+    });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+const { stdout } = process;
+
 const parseEnv = () => {
     // Write your code here 
+    const env = process.env;
+    for (let [key, value] of Object.entries(env)) {
+      if (key.includes("RSS_")) {
+        const output = `${key}=${value};`;
+        stdout.write(output + "\n");
+      }
+    }
 };
 
 parseEnv();
