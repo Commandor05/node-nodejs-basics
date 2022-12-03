4d218515ebf885b800b25e6dc47be2a5f0199c96
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2022-12-03 21:42:04 +0200

    feat: make fs delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,30 @@
+import { rm, access } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
     // Write your code here 
+  const fileName = 'fileToRemove.txt';
+  const destinationDir = 'files';
+  const destinationDirPath = path.join(__dirname, destinationDir);
+  const destinationFilePath = path.join(destinationDirPath, fileName);
+
+  try {
+    await checkFileAccess(destinationFilePath);
+    await rm(destinationFilePath)
+  } catch(e) {
+    console.log(e)
+  }
 };
+
+const checkFileAccess = async (filePath) => {
+  try {
+    await access(filePath);
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
+}
 
 await remove();
