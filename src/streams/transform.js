0f3a1276054d72764b8c8c2151f56ef72d1fce31
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2022-12-05 00:12:50 +0200

    feat: make streams transform task

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,28 @@
+import { stdout as output, stdin as input} from 'process';
+import { Transform, Readable } from 'stream';
+import readline from 'readline';
+
 const transform = async () => {
-    // Write your code here 
+    const revertTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, String(chunk).split("").reverse().join(""));
+        },
+    });
+
+    const readLine = readline.createInterface({ input, output });
+    const inStream = new Readable({
+        read() {}
+    });
+  
+    output.write(`Start typing. Any typed line will be reversed.\n`);
+  
+    readLine.on('line', (input) => {
+      inStream.push(input);
+      inStream.pipe(revertTransform).pipe(output);
+      readLine.close();
+    });
+
+    process.on('exit', () => output.write(`\n`));
 };
 
 await transform();
