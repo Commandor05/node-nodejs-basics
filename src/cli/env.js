97ecdf35a00352fa24c57c4037e11009f959d62c
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-06-16 12:01:00 +0300

    feat: add the solution for the cli section

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
+import Formatter from "../utils/formatter.js";
+
 const parseEnv = () => {
-    // Write your code here 
+  const selector = /^RSS_/;
+  const envFormatter = new Formatter(Formatter.formatTypes.env);
+  const envVarsAsArray = Object.entries(process.env);
+  const targetVarsArray = envVarsAsArray.filter((key, value) =>
+    selector.test(key)
+  );
+
+  process.stdout.write(envFormatter.format(targetVarsArray));
 };
 
 parseEnv();
