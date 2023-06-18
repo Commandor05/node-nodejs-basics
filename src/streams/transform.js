95472b6de0d46461ec2b646647bb9cd1765c5ab3
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-06-18 22:14:52 +0300

    feat: add the solution for the streams section

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,25 @@
+import { pipeline } from "stream/promises";
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  try {
+    process.stdout.write(`Start typing. Any typed line will be reversed.\n`);
+    process.stdout.write("For exit press 'ctrl + c'.\n");
+
+    const reversesText = new Transform({
+      transform(chunk, encoding, callback) {
+        callback(
+          null,
+          chunk.toString().replace(/\n$/, "").split("").reverse().join("") +
+            "\n"
+        );
+      },
+    });
+
+    await pipeline(process.stdin, reversesText, process.stdout);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,28 @@
+import { createWriteStream } from "node:fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+  const fileName = "fileToWrite.txt";
+  const targetDir = "files";
+  const targetDirPath = path.join(__dirname, targetDir);
+  const targetFilePath = path.join(targetDirPath, fileName);
+  try {
+    const writableSream = createWriteStream(targetFilePath);
+    process.stdout.write(
+      `Start typing. Any typed data will be written to file ${fileName}.\n`
+    );
+    process.stdout.write("For exit press 'ctrl + c'.\n");
+
+    process.stdin.on("data", (chank) => {
+      writableSream.write(chank);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await write();
