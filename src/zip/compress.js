896df85cec681af865a9932c48b705ca35b2acf9
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-06-19 21:49:56 +0300

    feat: add the solution for the zlib section

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,33 @@
+import { createReadStream, createWriteStream } from "node:fs";
+import { createGzip } from "node:zlib";
+import { pipeline } from "stream/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const sourceFileName = "fileToCompress.txt";
+  const targetFileName = "archive.gz";
+  const targetDir = "files";
+  const targetDirPath = path.join(__dirname, targetDir);
+  const sourceFilePath = path.join(targetDirPath, sourceFileName);
+  const targetFilePath = path.join(targetDirPath, targetFileName);
+
+  try {
+    const readableSream = createReadStream(sourceFilePath);
+    const writableSream = createWriteStream(targetFilePath);
+    const gzip = createGzip();
+
+    await pipeline(readableSream, gzip, writableSream);
+
+    process.stdout.write(
+      `File - "${sourceFileName}" was compressed to "${targetFileName}" in "/${targetDir}"\n`
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 await compress();
