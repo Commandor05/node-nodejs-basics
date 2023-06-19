ad8f139f4182f1a448c0362719f86906668df09f
Oleh Kaliuzhnyi <el_commandor@outlook.com>
2023-06-19 19:09:40 +0300

    feat: add the solution for the worker threads section

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,49 @@
+import os from "os";
+import { Worker } from "worker_threads";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpusCount = os.cpus().length;
+  const workerFilePath = path.join(__dirname, "worker.js");
+  const initialNumberForCalculations = 10;
+  const workersList = new Map();
+
+  for (let i = 0; i < cpusCount; i++) {
+    const worker = new Worker(workerFilePath);
+    const numberForCalculations = initialNumberForCalculations + i;
+
+    worker.postMessage(numberForCalculations);
+
+    const workerResult = await new Promise((resolve, reject) => {
+      worker.on("message", (message) => {
+        resolve({ status: "resolved", data: message });
+      });
+      worker.on("error", (error) => {
+        resolve({ status: "error", data: null });
+      });
+      worker.on("exit", (code) => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped with exit code ${code}`));
+        }
+      });
+    });
+
+    workersList.set(worker.threadId, workerResult);
+  }
+
+  console.log(`CPUs count: ${cpusCount}`);
+
+  for (const [workerId, workerResult] of workersList.entries()) {
+    process.stdout.write(
+      `(Worker ${workerId}) status: ${workerResult.status}  data: ${workerResult.data}\n`
+    );
+  }
+
+  process.exit();
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,19 @@
+import { parentPort } from "worker_threads";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  parentPort.on("message", (n) => {
+    // Uncomment rows below to check the working with errors
+    // if (n % 2 === 0) {
+    //   parentPort.postMessage(nthFibonacci(n));
+    // } else {
+    //   throw new Error("Error: Worker thread error!");
+    // }
+    parentPort.postMessage(nthFibonacci(n));
+  });
 };
 
 sendResult();
